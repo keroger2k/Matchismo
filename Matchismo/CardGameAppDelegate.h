a1938b014eff92491970e57efa69072909a51cf3
Kyle Rogers <kyle.rogers@gmail.com>
2013-08-28 15:03:18 -0500

    Initial Commit

---
+++ Matchismo/CardGameAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CardGameAppDelegate.h
+//  Matchismo
+//
+//  Created by Kyle Rogers on 8/28/13.
+//  Copyright (c) 2013 Kyle Rogers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardGameAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Matchismo/CardGameViewController.h
@@ -0,0 +1,13 @@
+//
+//  CardGameViewController.h
+//  Matchismo
+//
+//  Created by Kyle Rogers on 8/28/13.
+//  Copyright (c) 2013 Kyle Rogers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardGameViewController : UIViewController
+
+@end
