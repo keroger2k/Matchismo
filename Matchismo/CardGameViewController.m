b962c2788fbfe00adfeb18806b4f2ddabcdb1971
Kyle Rogers <kyle.rogers@gmail.com>
2013-08-29 16:09:33 -0500

    added label to storyboard

--- Matchismo/CardGameViewController.m
+++ Matchismo/CardGameViewController.m
@@ -16,6 +16,7 @@
 @property (strong, nonatomic) IBOutletCollection(UIButton) NSArray *cardButtons;
 @property (strong, nonatomic) CardMatchingGame *game;
 @property (weak, nonatomic) IBOutlet UILabel *scoreLabel;
+@property (weak, nonatomic) IBOutlet UILabel *messageLabel;
 
 @end
 
